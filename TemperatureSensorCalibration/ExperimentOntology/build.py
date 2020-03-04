fc0d673449a0dec946567a5070162690a8f1e532
Emil Nielsen <emilqz@gmail.com>
2020-03-04 15:57:01 +0100

    Ontology

--- TemperatureSensorCalibration/ExperimentOntology/build.py
+++ TemperatureSensorCalibration/ExperimentOntology/build.py
@@ -1,7 +1,53 @@
-from rdflib import Graph
+from rdflib import Literal
 
-g = Graph()
-g.parse('ThermistorSchema.ttl',format='turtle')
+from wrapper import *
+
+g = model()
+
+ESP32 = IOT5['/esp32/1']
+water1 = IOT5['/water/1']
+thermistor1 = IOT5['/thermistor/1']
+thermometerReal = IOT5['/thermometer/real']
+thermometerSimMin = IOT5['/thermometerSim/min']
+thermometerSimMax = IOT5['/thermometerSim/max']
+thermometerSimAvg = IOT5['/thermometerSim/avg']
+functionMin = IOT5['/function/min']
+functionMax = IOT5['/function/max']
+functionAvg = IOT5['/function/avg']
+
+g.add((ESP32, RDF.type, IOT5.ESP32))
+g.add((ESP32, IOT5.connectedTo, thermistor1))
+
+g.add((thermistor1, RDF.type, IOT5.Thermistor))
+g.add((thermistor1, FRAME.hasMeasurement, water1))
+
+g.add((thermometerReal, RDF.type, IOT5.Water))
+g.add((thermometerReal, FRAME.hasMeasurement, water1))
+
+g.add((functionMin, RDF.type, IOT5.Function))
+g.add((functionMin, IOT5.hasType, IOT5.LinearFunction))
+g.add((functionMin, BRICK.hasInput, thermistor1))
+g.add((functionMin, IOT5.a, Literal(0.836, datatype=XSD.float)))
+g.add((functionMin, IOT5.b, Literal(3.2044, datatype=XSD.float)))
+
+g.add((functionMax, RDF.type, IOT5.Function))
+g.add((functionMax, IOT5.hasType, IOT5.LinearFunction))
+g.add((functionMax, BRICK.hasInput, thermistor1))
+g.add((functionMax, IOT5.a, Literal(0.7984, datatype=XSD.float)))
+g.add((functionMax, IOT5.b, Literal(10.237, datatype=XSD.float)))
+
+g.add((functionAvg, RDF.type, IOT5.Function))
+g.add((functionAvg, IOT5.hasType, IOT5.LinearFunction))
+g.add((functionAvg, BRICK.hasInput, thermistor1))
+g.add((functionAvg, IOT5.a, Literal(0.8278, datatype=XSD.float)))
+g.add((functionAvg, IOT5.b, Literal(5.9526, datatype=XSD.float)))
+
+g.add((functionMin, BRICK.hasOutput, thermometerSimMin))
+g.add((functionMax, BRICK.hasOutput, thermometerSimMax))
+g.add((functionAvg, BRICK.hasOutput, thermometerSimAvg))
+
+
+g.serialize("ontology.ttl", 'turtle')
 
 def query(g, q):
     r = g.query(q)
--- TemperatureSensorCalibration/ExperimentOntology/wrapper.py
+++ TemperatureSensorCalibration/ExperimentOntology/wrapper.py
@@ -1,6 +1,5 @@
 from rdflib import Graph, Namespace
 
-
 # Standard namespaces that we can refer to
 # See: https://www.w3.org/TR/rdf-schema/
 RDF = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
@@ -10,28 +9,28 @@ RDFS = Namespace('http://www.w3.org/2000/01/rdf-schema#')
 OWL = Namespace('http://www.w3.org/2002/07/owl#')
 
 # See: https://brickschema.org/ontology
-BRICK = Namespace('https://brickschema.org/schema/1.1.0/Brick#')
+BRICK = Namespace('https://brickschema.org/schema/1.0.3/Brick#')
+FRAME = Namespace('https://brickschema.org/schema/1.0.3/BrickFrame#')
 
-IOT5 = Namespace('https://github.com/Xitric/IoT-Handins')
+IOT5 = Namespace('https://github.com/Xitric/IoT-Handins#')
+XSD = Namespace('http://www.w3.org/2001/XMLSchema#')
 
-# We define our own namespace to avoid naming conflicts
-# This is used to represent build information like git repository, branch, commit id, etc.
-BUILD = Namespace('https://github.com/Xitric/IoT-Exercises/releases/tag/v1.0.0/Metadata/Build#')
 
 def model():
     g = Graph()
 
-    g.parse('turtle/demo/Brick_expanded.ttl', format='turtle')
+    # g.parse('turtle/demo/Brick_expanded.ttl', format='turtle')
 
     # Read file describing our data types and relationships for build information
-    g.parse('turtle/buildinfo/Build_extension.ttl', format='turtle')
+    g.parse('ThermistorSchema.ttl', format='turtle')
 
     g.bind('rdf', RDF)
     g.bind('rdfs', RDFS)
     g.bind('owl', OWL)
     g.bind('brick', BRICK)
     g.bind('iot5', IOT5)
-    g.bind('build', BUILD)
+    g.bind('xsd', XSD)
+    g.bind('frame', FRAME)
 
     return g
 
@@ -39,5 +38,3 @@ def model():
 def query(g, q):
     r = g.query(q)
     return list(map(lambda row: list(row), r))
-
-
